2b161129a8c10517cb998cd7bf2b9091d1768b07
Jiri <mohandes.jiri@gmail.com>
2023-06-07 19:54:20 +0330

    Fix woof for firefox

--- About_files/notif.js
+++ About_files/notif.js
@@ -8,7 +8,28 @@ function wait(ms) {
     if (typeof $ === 'undefined') {
         return;
     }
+    $.fn.animateScale = function(startScale, endScale, duration, easing, complete){
+        return this.each(function(){
+            var elem = $(this);
+
+            $(this).css({ borderSpacing: startScale }).animate({borderSpacing: endScale}, {
+                duration: duration,
+                easing: easing,
+                step: function(now){
+                    elem.css({
+                        '-moz-transform':'scale('+now+')',
+                        '-webkit-transform':'scale('+now+')',
+                        '-o-transform':'scale('+now+')',
+                        '-ms-transform':'scale('+now+')',
+                        'transform':'scale('+now+')'
+                    });
+                },
+                complete: complete || $.noop
+            });
+        });
+    };
     window.showBonkNotif = async function (text) {
+        const isFirefox = window.navigator.userAgent.indexOf("Firefox") !== -1;
         const root = $('#BONKNOTIF_ROOT');
         const image = $('#BONKNOTIF_IMAGE');
         const textElement = $('#BONKNOTIF_TEXT');
@@ -34,17 +55,27 @@ function wait(ms) {
         await wait(1000);
         woof.play();
         await wait(70);
-        image.animate({
-            zoom: 1.2,
-        }, 200).animate({
-            zoom: 1,
-        }, 200);
-        await wait(600);
-        image.animate({
-            zoom: 1.2,
-        }, 200).animate({
-            zoom: 1,
-        }, 200);
+        if (isFirefox) {
+            image.animateScale(1, 1.2, 200);
+            await wait(200);
+            image.animateScale(1.2, 1, 200);
+            await wait(600);
+            image.animateScale(1, 1.2, 200);
+            await wait(200);
+            image.animateScale(1.2, 1, 200);
+        } else {
+            image.animate({
+                zoom: 1.2,
+            }, 200).animate({
+                zoom: 1,
+            }, 200);
+            await wait(600);
+            image.animate({
+                zoom: 1.2,
+            }, 200).animate({
+                zoom: 1,
+            }, 200);
+        }
         await wait(7000);
         root.animate({
             bottom: -150,
